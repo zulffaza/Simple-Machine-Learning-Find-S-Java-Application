061b2010eed3813e3c1b0c1d3f425e2688a5b968
Faza Zulfika P P <zulffaza@gmail.com>
2017-10-06 10:39:01 +0700

    Perubahan agar dapat membaca melalui file

--- src/example/finds/Main.java
+++ src/example/finds/Main.java
@@ -1,26 +1,95 @@
 package example.finds;
 
+import java.io.*;
+import java.util.ArrayList;
+
 /**
  * Aplikasi implementasi algoritma Find-S Machine Learning
+ * <p>
  *
- * Dibuat oleh Faza Zulfika Permana Putra - 2110151023 - D4 Teknik Informatika PENS 2015
- * Pada tanggal 15 September 2017
+ * @author Faza Zulfika P P - 2110151023 - D4 Teknik Informatika A PENS
+ * @version 1.3
+ * @since 14 September 2017
  */
 public class Main {
 
     /**
-     * Dataset attribute, minimal instance attribute adalah 2, satu instance dan satu target
+     * trainingAttributes merupakan data training untuk mencari hipotesa data
+     * testingAttributes merupakan data testing untuk mencari kesimpulan berdasarkan hipotesa yang ditemukan
      */
-    private static String[][] attributes = {
-            {"Sunny", "Warm", "Normal", "Strong", "Warm", "Same", "Yes"},
-            {"Sunny", "Warm", "High", "Strong", "Warm", "Same", "Yes"},
-            {"Rainy", "Cold", "High", "Strong", "Warm", "Change", "No"},
-            {"Sunny", "Warm", "High", "Strong", "Cool", "Change", "Yes"},
-    };
+    private static String[][] trainingAttributes, testingAttributes;
 
+    /**
+     * hypotesis merupakan hipotesa data berdasarkan training data yang digunakan
+     */
+    private static String[] hypotesis;
+
+    /**
+     * TRAINING_DATA_FILES merupakan lokasi data training
+     */
+    private static final String TRAINING_DATA_FILES = "G:\\Materi Semester 5\\Machine Learning\\Find-S\\Example-1\\data\\TrainingData.txt";
+
+    /**
+     * TESTING_DATA_FILES merupakan lokasi data testing
+     */
+    private static final String TESTING_DATA_FILES = "G:\\Materi Semester 5\\Machine Learning\\Find-S\\Example-1\\data\\TestingData.txt";
+
+    /**
+     * Merupakakan method main yang dijalankan saat program di jalankan
+     * Method ini akan mengambil data training dan data testing dari lokasi data
+     * Lalu mencari hipotesa berdasarkan data training dan mencari kesimpulan berdasarkan hipotesa dan data testing
+     *
+     * @param args merupakan parameter dari user saat program dijalankan
+     */
     public static void main(String[] args) {
+        trainingAttributes = setData(TRAINING_DATA_FILES);
+        testingAttributes = setData(TESTING_DATA_FILES);
+
         getHypotesis("Yes");
-        getHypotesis("No");
+//        getHypotesis("No");
+
+        testHypotesis();
+    }
+
+    /**
+     * Merupakan fungsi yang digunakan untuk membaca data dari dalam file dan mengconvertnya menjadi bentuk array of string dua dimensi
+     *
+     * @param filePath merupakan lokasi file yang ingin dibaca isinya, setiap kata pada file haruslah dipisahkan dengan spasi (" ")
+     * @return merupakan isi file dalam bentuk array of string dua dimensi
+     */
+    private static String[][] setData(String filePath) {
+        ArrayList<String[]> resultList = new ArrayList<>(); // List untuk menampung sementara isi file
+        String[][] attributes = null; // Array of string yang akan dikembalikan
+
+        try {
+            File file = new File(filePath); // Membuat object file dari file yang diinginkan
+            BufferedReader br = new BufferedReader(new FileReader(file)); // Membuat object untuk membaca file
+            String str = "";
+
+            /*
+             * Looping untuk membaca keseluruhan isi file
+             */
+            while (str != null) {
+                str = br.readLine(); // Mengambil setiap baris file
+
+                if (str != null) // Jika baris ada
+                    resultList.add(str.split(" ")); // Memisahkan setiap kata menurut spasi (" ")
+            }
+        } catch (FileNotFoundException e) { // Jika file tidak ditemukan
+            System.out.println("File tidak ditemukan");
+        } catch (IOException e) { // Jika terjadi kesalahan saat membaca file
+            System.out.println("Kesalahan saat membaca file");
+        } finally {
+            if (resultList.size() > 0) { // Jika file tidak kosong
+                int rowSize = resultList.size();
+                int columnSize = resultList.get(0).length;
+
+                attributes = new String[rowSize][columnSize]; // Membuat array of string berukuran data
+                attributes = resultList.toArray(attributes); // Menyimpan data dari list ke array of string
+            }
+        }
+
+        return attributes; // Mengembalikan data
     }
 
     /**
@@ -29,25 +98,25 @@ public class Main {
      * @param target adalah target yang diinginkan
      */
     private static void getHypotesis(String target) {
-        int attributesLength = attributes.length;
+        int attributesLength = trainingAttributes.length;
 
         System.out.println("Search hypotesis with target \"" + target + "\"...");
         System.out.println("");
 
         if (attributesLength > 0) { // Pengecekan apakah attribute ada isinya
-            int instanceLength = attributes[0].length;
+            int instanceLength = trainingAttributes[0].length;
 
             if (instanceLength > 1) { // Pengecekan apakah attribute minimal memiliki 2 instance
                 instanceLength--; // Jumlah instance dikurangi satu agar tidak membaca target
 
-                String[] hypotesis = new String[instanceLength]; // Membuat array hipotesis sebesar jumlah instance
+                hypotesis = new String[instanceLength]; // Membuat array hipotesis sebesar jumlah instance
                 int attributeFirstIndex = 0;
 
                 /*
                  * Pengambilan dataset pertama yang memiliki target yang diinginkan
                  */
                 for (int i = 0; i < attributesLength; i++) {
-                    if (attributes[i][instanceLength].equals(target)) {
+                    if (trainingAttributes[i][instanceLength].equals(target)) {
                         attributeFirstIndex = i;
                         break;
                     }
@@ -56,15 +125,15 @@ public class Main {
                 /*
                  * Menyimpan dataset pertama di array hipotesis
                  */
-                System.arraycopy(attributes[attributeFirstIndex], 0, hypotesis, 0, instanceLength);
+                System.arraycopy(trainingAttributes[attributeFirstIndex], 0, hypotesis, 0, instanceLength);
 
                 /*
                  * Proses membuat instance yang berbeda disetiap dataset mejadi '?'
                  */
                 for (int i = 1; i < attributesLength; i++) {
-                    if (attributes[i][instanceLength].equals(target)) {
+                    if (trainingAttributes[i][instanceLength].equals(target)) {
                         for (int j = 0; j < instanceLength; j++) {
-                            if (!hypotesis[j].equals(attributes[i][j]))
+                            if (!hypotesis[j].equals(trainingAttributes[i][j]))
                                 hypotesis[j] = "?";
                         }
                     }
@@ -81,5 +150,45 @@ public class Main {
                 System.out.println("Instance must over than 1, and the last column mus be target...");
         } else
             System.out.println("Attribute is empty...");
+    }
+
+    /**
+     * Merupakan method yang digunakan untuk mencari kesimpulan dari data testing berdasarkan hipotesa yang didapat
+     */
+    private static void testHypotesis() {
+        boolean isSame; // Untuk mengecek apakah hipotesa sama dengan data testing
+
+        /*
+         * Mengecek keseluruhan data testing
+         */
+        for (String[] testingArray : testingAttributes) {
+            System.out.print("Testing data : ");
+
+            isSame = true; // Mengasumsikan hipotesa mirip dengan data testing
+
+            /*
+             * Mengecek setiap attribute pada data testing
+             */
+            for (int i = 0; i < testingArray.length; i++) {
+                String attribute = testingArray[i]; // Mengambil attribute
+
+                /*
+                 * Mengecek apakah data hipotesa mirip dengan data testing
+                 */
+                if (!hypotesis[i].equals("?") && !attribute.equals(hypotesis[i]))
+                    isSame = false;
+
+                attribute += (i == (testingArray.length - 1) ? " " : ", ");
+                System.out.print(attribute);
+            }
+
+            /*
+             * Mengecek kesimpulan dan menampilkan
+             */
+            if (isSame)
+                System.out.println("= Yes");
+            else
+                System.out.println("= No");
+        }
     }
 }
